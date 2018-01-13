704e46637256314bcd33226627d90f7a0751b783
Mateusz Maciaszek <maciaszek96@gmail.com>
2018-01-13 02:20:14 +0100

    add post methods for doctor

--- src/main/java/com/example/doctormanagement/rest/DoctorController.java
+++ src/main/java/com/example/doctormanagement/rest/DoctorController.java
@@ -6,6 +6,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.example.doctormanagement.daoimplementation.JDBCDoctorDao;
 import com.example.doctormanagement.daointerface.DoctorDao;
 import com.example.doctormanagement.model.Doctor;
+import com.example.doctormanagement.model.Patient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,5 +28,20 @@ public class DoctorController {
                                    @RequestParam(value="surname", defaultValue="") String surname) {
         return doctorDao.getDoctorByNameSurname(name,surname);
         //example: http://localhost:8080/getDoctorByName?name=Anna&surname=Nowak
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addPatientToDoctor")
+    public String addPatientToDoctor(@RequestParam(value="id", defaultValue="1") String doctorId,
+                           @RequestBody Patient patient) {
+        doctorDao.addPatient(Integer.parseInt(doctorId), patient);
+        return "OK";
+        //example: http://localhost:8080/addPatientToDoctor?id=1
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addDoctor")
+    public String addDoctor(@RequestBody Doctor doctor) {
+        doctorDao.addDoctor(doctor);
+        return "OK";
+        //example: http://localhost:8080/addDoctor
     }
 }
--- src/main/java/com/example/doctormanagement/rest/PatientController.java
+++ src/main/java/com/example/doctormanagement/rest/PatientController.java
@@ -2,7 +2,10 @@ package com.example.doctormanagement.rest;
 
 import com.example.doctormanagement.daoimplementation.JDBCPatientDao;
 import com.example.doctormanagement.daointerface.PatientDao;
+import com.example.doctormanagement.model.Doctor;
 import com.example.doctormanagement.model.Patient;
+import com.example.doctormanagement.model.Prescription;
+import com.example.doctormanagement.model.Treatment;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
@@ -26,5 +29,21 @@ public class PatientController {
                                     @RequestParam(value = "surname", defaultValue = "") String surname) {
         return patientDao.getPatientByNameSurname(name, surname);
         //example: http://localhost:8080/getPatientByName?name=Jan&surname=Kowalski
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addTreatment")
+    public String addTreatment(@RequestParam(value = "id", defaultValue = "1") String patientId,
+                               @RequestBody Treatment treatment) {
+        patientDao.addTreatment(Integer.parseInt(patientId), treatment);
+        return "OK";
+        //example: http://localhost:8080/addTreatment
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addTreatment")
+    public String addPrescription(@RequestParam(value = "id", defaultValue = "1") String patientId,
+                                  @RequestBody Prescription prescription) {
+        patientDao.addPrescription(Integer.parseInt(patientId), prescription);
+        return "OK";
+        //example: http://localhost:8080/addPrescription
     }
 }
